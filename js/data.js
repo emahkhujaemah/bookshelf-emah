e437fb31746a67bf0981ac0b3e39122a87753078
Emah Khujaemah <khuza.emah24@gmail.com>
2021-08-15 10:59:16 +0700

    all file

---
+++ js/data.js
@@ -0,0 +1,79 @@
+const STORAGE_KEY = "BOOKSHELF_APPS";
+
+let books = [];
+
+function isStorageExist() /* Boolean */ {
+    if(typeof(Storage) === undefined){
+        alert("Browser kamu tidak mendukung local storage ");
+        return false;
+    }
+    return true;
+}
+
+function saveData(){
+    const parsed = JSON.stringify(books);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event("ondatasaved"));
+}
+
+function loadDataFromStorage(){
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+
+    let data = JSON.parse(serializedData);
+
+    if(data !== null)
+        books = data;
+
+    document.dispatchEvent(new Event("ondataloaded"));
+}
+
+function updateDataToStorage(){
+    if(isStorageExist())
+        saveData();
+}
+
+function composeBookObject(judul, penulis, tahun, selesaiDibaca){
+    return {
+        id: +new Date(),
+        judul,
+        penulis,
+        tahun, 
+        selesaiDibaca
+    };
+}
+
+function findBook(bookId){
+    for(book of books){
+        if(book.id === bookId)
+        return book;
+    }
+    return null;
+}
+
+function findBookIndex(bookId){
+    let index = 0;
+    for(book of books){
+        if(book.id === bookId)
+        return index;
+
+        index++;
+    }
+    return -1;
+}
+
+function refreshDataFromBooks() {
+    const incompleteBookshelfList = document.getElementById(INCOMPLETE_BOOKSHELF_LIST_ID);
+    const completeBookshelfList = document.getElementById(COMPLETE_BOOKSHELF_LIST_ID);
+
+    for(book of books) {
+        const bukuBaru = makeBook(book.judul,  `penulis: ${book.penulis}`, `Tahun: ${book.tahun}`, book.selesaiDibaca);
+
+        bukuBaru[BOOK_ITEMID] = book.id;
+
+        if(book.selesaiDibaca){
+            completeBookshelfList.append(bukuBaru);
+        }else{
+            incompleteBookshelfList.append(bukuBaru);
+        }
+    }
+}
